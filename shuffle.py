92ccb68248d9bd17134ba3accdc49a74252d9238
Kyle Barron <kylebarron2@gmail.com>
2022-05-04 21:19:38 -0600

    Keep within __main__ https://github.com/dask/distributed/issues/2520#issuecomment-470817810

--- shuffle.py
+++ shuffle.py
@@ -6,20 +6,29 @@ import dask_geopandas
 import pygeos
 from distributed import Client
 
-client = Client()
-print(f'Dashboard link: {client.dashboard_link}')
 
-path = 'data/*.parquet'
-ddf = dd.read_parquet(path, columns=['geometry'], split_row_groups=True)
-ddf['geometry'] = ddf.map_partitions(lambda part: pygeos.from_wkb(part.geometry), meta=('geometry', object))
-ddf = dask_geopandas.from_dask_dataframe(ddf)
+def main():
+    path = "data/*.parquet"
+    ddf = dd.read_parquet(path, columns=["geometry"], split_row_groups=True)
+    ddf["geometry"] = ddf.map_partitions(
+        lambda part: pygeos.from_wkb(part.geometry), meta=("geometry", object)
+    )
+    ddf = dask_geopandas.from_dask_dataframe(ddf)
 
-start_time = time()
-shuffled = ddf.spatial_shuffle(by='hilbert', npartitions=None)
-end_time = time()
-print(f'Time to shuffle data: {(end_time - start_time) / 60:.2f} minutes')
+    start_time = time()
+    shuffled = ddf.spatial_shuffle(by="hilbert", npartitions=None)
+    end_time = time()
+    print(f"Time to shuffle data: {(end_time - start_time) / 60:.2f} minutes")
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            "ignore", message=".*initial implementation of Parquet.*"
+        )
+        shuffled.to_parquet("shuffled.parquet")
 
 
-with warnings.catch_warnings():
-    warnings.filterwarnings('ignore', message='.*initial implementation of Parquet.*')
-    shuffled.to_parquet('shuffled.parquet')
+if __name__ == "__main__":
+    client = Client()
+    print(f"Dashboard link: {client.dashboard_link}")
+    main()
+    print("done")
